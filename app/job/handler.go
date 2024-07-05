22aa385af1ea3ea92fd72fc5db7bdd34dbae81c2
seakee <seakee23@gmail.com>
2024-07-05 17:03:47 +0800

    stop job ipMonitor

--- app/job/handler.go
+++ app/job/handler.go
@@ -1,7 +1,6 @@
 package job
 
 import (
-	"github.com/seakee/dockmon/app/job/monitor"
 	"github.com/seakee/dockmon/app/pkg/schedule"
 	"github.com/sk-pkg/feishu"
 	"github.com/sk-pkg/logger"
@@ -11,6 +10,6 @@ import (
 
 func Register(logger *logger.Manager, redis map[string]*redis.Manager, db map[string]*gorm.DB, feishu *feishu.Manager, s *schedule.Schedule) {
 	// Monitor broadband public network IP changes
-	ipMonitor := monitor.NewIpMonitor(logger, redis["dockmon"])
-	s.AddJob("IpMonitor", ipMonitor).PerMinuit(5).WithoutOverlapping()
+	// ipMonitor := monitor.NewIpMonitor(logger, redis["dockmon"])
+	// s.AddJob("IpMonitor", ipMonitor).PerMinuit(5).WithoutOverlapping()
 }
